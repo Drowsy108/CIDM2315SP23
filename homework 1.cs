3785d123daed77203635b527e7fa40dd0da98c4f
Drowsy108 <lesalazar1@buffs.wtamu.edu>
2025-01-23 03:17:55 -0600

    Add files via upload
    homework 1 code file

---
+++ homework 1.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+            double X = 2.5; // im not to sure as to why I cant use decimal instead of double?
+            double Y = 3.3;
+            double Z = 4 * X * X + 3 * Y;
+            Console.WriteLine($"When X = {X} and Y = {Y}, Z = {Z:F2}");
+    }
+}
